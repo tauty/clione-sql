f1aa1ec988a5810e319131507b32b54c7cdb331a
tetsuo.ohta <tetsuo.ohta@gmail.com>
2011-02-14 00:13:19 +0000

    changing...

--- clione-sql/src/tetz42/clione/SQLExecutor.java
+++ clione-sql/src/tetz42/clione/SQLExecutor.java
@@ -1,21 +1,15 @@
 package tetz42.clione;
 
 import static tetz42.clione.SQLManager.*;
-import static tetz42.clione.util.ClioneUtil.*;
 
 import java.io.InputStream;
-import java.lang.reflect.Field;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import tetz42.clione.exception.WrapException;
 import tetz42.clione.gen.SQLGenerator;
 import tetz42.clione.node.LineNode;
 import tetz42.clione.parsar.SQLParser;
@@ -59,9 +53,20 @@ public class SQLExecutor {
 
 	public Map<String, Object> find(Map<String, Object> paramMap)
 			throws SQLException {
-		// TODO 後でIterator対応したら、そちらにロジックを変更(Fetch 1 で呼び出す)
-		List<Map<String, Object>> list = this.findAll(paramMap);
-		return list.size() != 0 ? list.get(0) : null;
+		try {
+			for (Map<String, Object> map : SQLIterator.genIterator(this,
+					Map.class, paramMap)) {
+				return map;
+			}
+			return null;
+		} catch (SQLException e) {
+			throw new SQLException(
+					e.getMessage() + "\n\tsql:" + this.sqlGenerator.sql
+							+ "\n\t" + this.sqlGenerator.params,
+					e.getSQLState(), e.getErrorCode(), e);
+		} finally {
+			closeStatement();
+		}
 	}
 
 	public List<Map<String, Object>> findAll() throws SQLException {
@@ -76,19 +81,13 @@ public class SQLExecutor {
 	public List<Map<String, Object>> findAll(Map<String, Object> paramMap)
 			throws SQLException {
 
-		ArrayList<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
 		try {
-			stmt = this.genStmt(paramMap);
-			rs = stmt.executeQuery();
-			ResultSetMetaData md = rs.getMetaData();
-
-			while (rs.next()) {
-				Map<String, Object> map = new LinkedHashMap<String, Object>();
-				for (int i = 1; i <= md.getColumnCount(); i++) {
-					map.put(md.getColumnLabel(i), rs.getObject(i));
-				}
+			ArrayList<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+			for (Map<String, Object> map : SQLIterator.genIterator(this,
+					Map.class, paramMap)) {
 				list.add(map);
 			}
+			return list;
 		} catch (SQLException e) {
 			throw new SQLException(
 					e.getMessage() + "\n\tsql:" + this.sqlGenerator.sql
@@ -97,7 +96,6 @@ public class SQLExecutor {
 		} finally {
 			this.closeStatement();
 		}
-		return list;
 	}
 
 	public <T> T find(Class<T> entityClass) throws SQLException {
@@ -111,9 +109,19 @@ public class SQLExecutor {
 
 	public <T> T find(Class<T> entityClass, Map<String, Object> paramMap)
 			throws SQLException {
-		// TODO 後でIterator対応したら、そちらにロジックを変更(Fetch 1 で呼び出す)
-		List<T> list = this.findAll(entityClass, paramMap);
-		return list.size() != 0 ? list.get(0) : null;
+		try {
+			for (T entity : iteration(entityClass, paramMap)) {
+				return entity;
+			}
+			return null;
+		} catch (SQLException e) {
+			throw new SQLException(
+					e.getMessage() + "\n\tsql:" + this.sqlGenerator.sql
+							+ "\n\t" + this.sqlGenerator.params,
+					e.getSQLState(), e.getErrorCode(), e);
+		} finally {
+			this.closeStatement();
+		}
 	}
 
 	public <T> List<T> findAll(Class<T> entityClass) throws SQLException {
@@ -128,54 +136,25 @@ public class SQLExecutor {
 	public <T> List<T> findAll(Class<T> entityClass,
 			Map<String, Object> paramMap) throws SQLException {
 
-		// TODO 後でIterator対応したら、そちらにロジックを変更
 		ArrayList<T> list = new ArrayList<T>();
 		try {
-			stmt = this.genStmt(paramMap);
-			rs = stmt.executeQuery();
-			ResultSetMetaData md = rs.getMetaData();
-			Class<?> clazz = entityClass;
-			HashMap<String, FSet> fieldMap = new HashMap<String, FSet>();
-			while (clazz != null && clazz != Object.class) {
-				Field[] fields = clazz.getDeclaredFields();
-				for (Field field : fields) {
-					if (fieldMap.containsKey(field.getName()))
-						continue;
-					fieldMap.put(field.getName(),
-							FSet.genFSet(field, field.isAccessible()));
-				}
-				clazz = clazz.getSuperclass();
+			for (T entity : iteration(entityClass, paramMap)) {
+				list.add(entity);
 			}
-
-			while (rs.next()) {
-				T instance = entityClass.newInstance();
-				for (int i = 1; i <= md.getColumnCount(); i++) {
-					FSet fset = fieldMap.get(md.getColumnLabel(i));
-					if (fset == null)
-						fset = fieldMap.get(conv(md.getColumnLabel(i)));
-					if (fset == null)
-						continue;
-					fset.f.setAccessible(true);
-					fset.f.set(instance, getSQLData(fset.f, rs, i));
-					fset.f.setAccessible(fset.b);
-				}
-				list.add(instance);
-			}
+			return list;
 		} catch (SQLException e) {
 			throw new SQLException(
 					e.getMessage() + "\n\tsql:" + this.sqlGenerator.sql
 							+ "\n\t" + this.sqlGenerator.params,
 					e.getSQLState(), e.getErrorCode(), e);
-		} catch (InstantiationException e) {
-			throw new WrapException(entityClass.getSimpleName()
-					+ " must have default constructor.", e);
-		} catch (IllegalAccessException e) {
-			throw new WrapException(entityClass.getSimpleName()
-					+ " have security problem.", e);
 		} finally {
 			this.closeStatement();
 		}
-		return list;
+	}
+
+	public <T> SQLIterator<T> iteration(Class<T> entityClass,
+			Map<String, Object> paramMap) throws SQLException {
+		return SQLIterator.genIterator(this, entityClass, paramMap);
 	}
 
 	public int update() throws SQLException {
@@ -229,8 +208,7 @@ public class SQLExecutor {
 		return sql;
 	}
 
-	PreparedStatement genStmt(Map<String, Object> paramMap)
-			throws SQLException {
+	PreparedStatement genStmt(Map<String, Object> paramMap) throws SQLException {
 		stmt = manager.con().prepareStatement(genSql(paramMap));
 		manager.putExecutor(this);
 		int i = 1;
@@ -238,31 +216,5 @@ public class SQLExecutor {
 			stmt.setObject(i++, param);
 		}
 		return stmt;
-	}
-
-	private Object conv(String columnLabel) {
-		String[] strings = columnLabel.toLowerCase().split("_");
-		StringBuilder sb = new StringBuilder();
-		for (String s : strings) {
-			if (s.length() == 0)
-				continue;
-			if (sb.length() == 0)
-				sb.append(s);
-			else
-				sb.append(s.substring(0, 1).toUpperCase() + s.substring(1));
-		}
-		return sb.toString();
-	}
-
-	private static class FSet {
-		Field f;
-		boolean b;
-
-		static FSet genFSet(Field f, boolean b) {
-			FSet fset = new FSet();
-			fset.f = f;
-			fset.b = b;
-			return fset;
-		}
 	}
 }
