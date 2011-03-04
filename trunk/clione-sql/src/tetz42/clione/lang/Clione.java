c61bf852ed30a24d3f5f729941d776243307c879
tetsuo.ohta@gmail.com <tetsuo.ohta@gmail.com@90886ef9-45e2-2e41-1d88-308523f0d563>
2011-03-04 12:06:01 +0000

    Changing...

--- trunk/clione-sql/src/tetz42/clione/lang/Clione.java
+++ trunk/clione-sql/src/tetz42/clione/lang/Clione.java
@@ -3,19 +3,16 @@ package tetz42.clione.lang;
 import tetz42.clione.util.ParamMap;
 
 public abstract class Clione {
-	
+
 	private Clione child;
-	
-	public void setChild(Clione child){
+
+	public void setChild(Clione child) {
 		this.child = child;
 	}
-	
-	protected Egg deliver(ParamMap paramMap){
-		if(child != null){
-			return child.perform(paramMap);
-		}
-		return new Egg();
+
+	protected Egg deliver(ParamMap paramMap) {
+		return child == null ? new Egg() : child.perform(paramMap);
 	}
-	
+
 	public abstract Egg perform(ParamMap paramMap);
 }
