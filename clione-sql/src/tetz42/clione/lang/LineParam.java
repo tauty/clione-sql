f6497139987f2221d5d5586c0e3dfc33d08b376b
tetsuo.ohta <tetsuo.ohta@gmail.com>
2011-04-04 02:08:53 +0000

    Changing...

--- clione-sql/src/tetz42/clione/lang/LineParam.java
+++ clione-sql/src/tetz42/clione/lang/LineParam.java
@@ -2,9 +2,6 @@ package tetz42.clione.lang;
 
 public class LineParam extends AbstractParam {
 	
-	// for ObjDumper4j. TODO Delete this line.
-	private static String dummy = "dummy";
-
 	public LineParam(String key, boolean isNegative) {
 		super(key, isNegative);
 	}
