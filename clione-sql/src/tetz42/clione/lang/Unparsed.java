fb41ef4bbd71e69d70f51ca26e0eb0f810cad683
tetsuo.ohta <tetsuo.ohta@gmail.com>
2011-06-15 09:43:39 +0000

    %INCLUDE

--- clione-sql/src/tetz42/clione/lang/Unparsed.java
+++ clione-sql/src/tetz42/clione/lang/Unparsed.java
@@ -1,25 +1,14 @@
 package tetz42.clione.lang;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import tetz42.clione.lang.func.ClioneFunction;
 import tetz42.clione.util.ParamMap;
 
 public class Unparsed extends ClioneFunction {
 
-	static ThreadLocal<List<Unparsed>> unparsedList = new ThreadLocal<List<Unparsed>>() {
-		@Override
-		protected List<Unparsed> initialValue() {
-			return new ArrayList<Unparsed>();
-		};
-	};
-
 	private String unparsedStr;
 
 	public Unparsed(String unparsedStr) {
 		this.unparsedStr = unparsedStr;
-		unparsedList.get().add(this);
 	}
 
 	@Override
