f08ccdafc521a5901ea4e1f35e9c2f8e6b376d06
tetsuo.ohta <tetsuo.ohta@gmail.com>
2012-03-28 11:21:04 +0000

    csv parsar commit

---
+++ clione-sql/src/tetz42/util/exception/UnknownFormatException.java
@@ -0,0 +1,23 @@
+package tetz42.util.exception;
+
+public class UnknownFormatException extends RuntimeException {
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private static final String CRLF = System.getProperty("line.separator");
+
+	public UnknownFormatException(String msg) {
+		super(msg);
+	}
+
+	public UnknownFormatException(Throwable e) {
+		super(e.getMessage(), e);
+	}
+
+	public UnknownFormatException(String msg, Throwable e) {
+		super(msg + CRLF + e.getMessage(), e);
+	}
+
+}
