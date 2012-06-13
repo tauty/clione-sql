5108d26223b9c622413428ea39c63633e366ebcc
tetsuo.ohta@gmail.com <tetsuo.ohta@gmail.com@90886ef9-45e2-2e41-1d88-308523f0d563>
2012-06-13 11:55:30 +0000

    developing the security check of '%SQL!'

--- trunk/clione-sql/src/tetz42/clione/lang/dialect/PostgresDialect.java
+++ trunk/clione-sql/src/tetz42/clione/lang/dialect/PostgresDialect.java
@@ -1,6 +1,6 @@
 package tetz42.clione.lang.dialect;
 
-public class MysqlDialect extends Dialect{
+public class PostgresDialect extends Dialect {
 	@Override
 	public boolean backslashWorkAsEscape() {
 		return true;
